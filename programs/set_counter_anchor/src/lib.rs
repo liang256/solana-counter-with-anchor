0f3edae7e458ce1ffe138e4a992fc1b5b732235a
LIANGYU <n795113@gmail.com>
2022-06-08 17:35:05 +0800

    implement set counter in anchor

--- programs/set_counter_anchor/src/lib.rs
+++ programs/set_counter_anchor/src/lib.rs
@@ -7,9 +7,46 @@ pub mod set_counter_anchor {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        state.counter = 0;
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Update>) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        state.counter = state.counter.checked_sub(1).unwrap_or(0);
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        state.counter = state.counter.checked_add(1).unwrap_or(u32::MAX);
+        Ok(())
+    }
+
+    pub fn set(ctx: Context<Update>, value: u32) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        state.counter = value;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 4)]
+    pub state: Account<'info, StateAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub state: Account<'info, StateAccount>
+}
+
+#[account]
+pub struct StateAccount {
+    pub counter: u32
+}
--- tests/set_counter_anchor.ts
+++ tests/set_counter_anchor.ts
@@ -1,5 +1,6 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { assert } from "chai";
 import { SetCounterAnchor } from "../target/types/set_counter_anchor";
 
 describe("set_counter_anchor", () => {
@@ -8,9 +9,55 @@ describe("set_counter_anchor", () => {
 
   const program = anchor.workspace.SetCounterAnchor as Program<SetCounterAnchor>;
 
+  const stateAccount = anchor.web3.Keypair.generate();
+
+  const setValue = 123; // can be any u32 number
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.rpc.initialize({
+      accounts: {
+        state: stateAccount.publicKey,
+        user: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+      },
+      signers: [stateAccount],
+    });
+
+    const state = await program.account.stateAccount.fetch(stateAccount.publicKey);
+    assert.equal(state.counter, 0);
+  });
+
+  it("set value", async () => {
+    await program.rpc.set(new anchor.BN(setValue), {
+      accounts: {
+        state: stateAccount.publicKey
+      }
+    });
+
+    const state = await program.account.stateAccount.fetch(stateAccount.publicKey);
+    assert.equal(state.counter, setValue);
+  });
+
+  it("increment", async () => {
+    await program.rpc.increment({
+      accounts: {
+        state: stateAccount.publicKey
+      }
+    });
+
+    const state = await program.account.stateAccount.fetch(stateAccount.publicKey);
+    assert.equal(state.counter, setValue + 1);
+  });
+
+  it("decrement", async () => {
+    await program.rpc.decrement({
+      accounts: {
+        state: stateAccount.publicKey
+      }
+    });
+
+    const state = await program.account.stateAccount.fetch(stateAccount.publicKey);
+    assert.equal(state.counter, setValue);
   });
 });
